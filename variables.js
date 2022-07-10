77bce57f34128c962e06616b7214c3afc2613a86
Cristian_Gomez26 <porte20k7@gmail.com>
2022-07-10 17:26:01 -0300

    mi cuarto repo
    mi cuarto repositorio JS

---
+++ variables.js
@@ -0,0 +1,18 @@
+'use strict' //Modo estricto de JavaScript
+
+// Declaracion de las variables Nombre Y Apellido
+var name = "Julian";
+var surname = "Alvarez";
+var fullName = name+" "+surname
+
+// Declaracion de la Edad y El Año De Nacimiento
+var age = 22
+var year = 2000
+// Alerta para el Usuario
+alert("Procesando Su Solicitud")
+
+//Imprime en el documento el Nombre Completo Y El Año De Nacimiento
+document.write("Nombre Completo: "+ fullName + "<br>" + "Año De Nacimiento: "+ year)
+
+
+
